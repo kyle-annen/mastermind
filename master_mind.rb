7353b28f86135890a074f2d4af7db5934a70c6e3
Kyle Annen <kyleannen@gmail.com>
2015-06-12 01:00:31 -0700

    class in progress

--- master_mind.rb
+++ master_mind.rb
@@ -1,5 +1,33 @@
-#Master Mind for The Odin Project
-This program was created for the odin project course track. 
+class MasterMind
 
-##To Play
-will be updated once complete
+  def initialize
+    @numbers, @guess, @board, @turn, @guess_hist, @result, @result_hist = [1,2,3,4,5,6], [], [1,2,3,4,5,6], 0, {}, [], {}
+  end
+
+  def new_game
+  end
+
+  def draw_board(array)
+    print "| "
+    0.upto(5) {|i| print i == 4 ? array[i] : array[i], " | "}
+  end
+
+  def gen_numbers
+  end
+
+  def get_guess
+    0.upto(5) do |i|
+      puts "Guess for #{i+1}:"
+      @guess[i] = gets.chomp.to_i
+      until @numbers.include? @guess[i]
+        puts "You must select integer 1-6:"
+        @guess[i] = gets.chomp.to_i
+      end
+    end
+    @turn += 1; round = "r" << @turn.to_s; @guess_hist[round] = @guess #error in hash addition, fix
+  end
+
+  def check_guess
+
+  end
+end
